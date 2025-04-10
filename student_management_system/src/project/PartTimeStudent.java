e9a3c0373428e6abc21d76f3bd3e71484fc0671f
Nilay Raval <100033937+NILAYRAVAL@users.noreply.github.com>
2025-04-10 23:26:50 +0530

    Update PartTimeStudent.java

--- student_management_system/src/project/PartTimeStudent.java
+++ student_management_system/src/project/PartTimeStudent.java
@@ -3,10 +3,12 @@ package project;
 import java.time.LocalDate;
 
 public record PartTimeStudent(String name, int age, String studentId, StudentStatus status, LocalDate enrollmentDate) implements Student {
+
     @Override
     public void printInfo() {
+        // Display the part-time student type
         System.out.printf(LanguageSupport.getString("student.info"),
-                name, age, studentId, status, enrollmentDate);
+                name, age, studentId, status, enrollmentDate, "Part-Time");
         System.out.println();
     }
 }
