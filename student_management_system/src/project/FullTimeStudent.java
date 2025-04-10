cf8df801cd0cadff43fd56f5e86daba3e2945a79
Nilay Raval <100033937+NILAYRAVAL@users.noreply.github.com>
2025-04-10 23:23:25 +0530

    Update FullTimeStudent.java

--- student_management_system/src/project/FullTimeStudent.java
+++ student_management_system/src/project/FullTimeStudent.java
@@ -3,10 +3,12 @@ package project;
 import java.time.LocalDate;
 
 public record FullTimeStudent(String name, int age, String studentId, StudentStatus status, LocalDate enrollmentDate) implements Student {
+
     @Override
     public void printInfo() {
+        // Display the full-time student type
         System.out.printf(LanguageSupport.getString("student.info"),
-                name, age, studentId, status, enrollmentDate);
+                name, age, studentId, status, enrollmentDate, "Full-Time");
         System.out.println();
     }
 }
