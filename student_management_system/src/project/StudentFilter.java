598e0a6a1755294a791fc35ed604e8f06f7edb05
Nilay Raval <100033937+NILAYRAVAL@users.noreply.github.com>
2025-04-10 23:29:51 +0530

    Update StudentFilter.java

--- student_management_system/src/project/StudentFilter.java
+++ student_management_system/src/project/StudentFilter.java
@@ -4,15 +4,23 @@ import java.util.function.Predicate;
 
 public class StudentFilter {
 
-	public static Predicate<Student> isActive() {
-		return student -> student.status() == StudentStatus.ACTIVE;
-	}
+    public static Predicate<Student> isActive() {
+        return student -> student.status() == StudentStatus.ACTIVE;
+    }
 
-	public static Predicate<Student> isInactive() {
-		return student -> student.status() == StudentStatus.INACTIVE;
-	}
+    public static Predicate<Student> isInactive() {
+        return student -> student.status() == StudentStatus.INACTIVE;
+    }
 
-	public static Predicate<Student> isGraduated() {
-		return student -> student.status() == StudentStatus.GRADUATED;
-	}
+    public static Predicate<Student> isGraduated() {
+        return student -> student.status() == StudentStatus.GRADUATED;
+    }
+
+    public static Predicate<Student> isFullTime() {
+        return student -> student instanceof FullTimeStudent;
+    }
+
+    public static Predicate<Student> isPartTime() {
+        return student -> student instanceof PartTimeStudent;
+    }
 }
